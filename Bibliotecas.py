381d7366a8d2dccdc1353106d67f73c8dd33b484
Aragao05 <130008592+Aragao05@users.noreply.github.com>
2024-04-19 19:32:27 -0400

    Modulo 2
    Finalização do modulo de Bibliotecas e começo do modulo de Funções

--- Bibliotecas.py
+++ Bibliotecas.py
@@ -120,4 +120,11 @@
 # 10.Faça um programa para uma loja que vende grama para jardins. Essa loja trabalha com jardins circulares e o preço do metro quadrado
 # da grama é de R$ 25,00. Peça à pessoa usuária o raio da área circular e devolva o valor em reais do quanto precisará pagar.
 
+from math import pi, pow
 
+raio = float(input("Insira qual o raio da circunferencia para saber o valor que vai pagar: "))
+area = pi * pow(raio,2)
+valor = area * 25.00
+
+print(f"Você precisará pagar R$ {round(valor,2)} por uma área de {round(area,2)} metros de grama")    # round(numero fracional, quantidade de casas
+# depois da virgula), round fromata um numero de forma que voce escolhe quantas casas quer veer depois da virgula.
---
+++ Funções.py
@@ -0,0 +1,40 @@
+# Built in functions  são funções que estão dentro do python e podem ser chamadas a qualquer momento EX: print, type, list
+
+#notas = {'1° Trimestre': 8.5, '2° Trimestre': 9.5, '3° trimestre': 7}
+
+# calculandoa  soma do meto normal
+#soma = 0
+#for nota in notas.values():  # .values() pega todos os numeros do dicionario e trasnforma eles em uma lista iteravel
+#    soma  += nota
+#print(soma)
+
+# calculando a soma usando uma Built in functions
+#print(sum(notas.values()))  # sum() soma os numeros de uma lista
+
+
+# calculando a media
+
+#qtd_notas = len(notas) # len retona a quantidade de intens de algo 
+#media = soma / qtd_notas
+#print(round(media,2))
+
+
+# fromato de uma função criada:
+#def <nome>(<parametros>,<quantos quiser>):
+#   <instruções>
+
+# para utilizar a função voce precisa chamar ela passando os parametros que ela requer
+#<nome>(<parametro_1>, <paremetro_2>)
+
+# calcular a media de um estudante, sendo possivel aumentar e diminuir a quantidade de notas que ele tem
+
+#notas = [8.5, 9.0, 6.0, 10.0]
+
+#def media(lista):
+#    calculo = sum(lista) / len(lista)
+#    print(round(calculo,2))
+
+#media(notas)
+
+
+
