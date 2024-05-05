a9349ff94a38ea7921d54c9b510df1c27d18b1cd
Aragao05 <130008592+Aragao05@users.noreply.github.com>
2024-05-05 19:20:21 -0400

    Inicio e termino do modulo Lidando com exceções
    Exercicio do modulo de estrutura de dados feito, começo e termino do conteudo do modulo lidando com exceções

--- Estruturas de dados.py
+++ Estruturas de dados.py
@@ -253,8 +253,26 @@ print(rotulos)
 # quantidade pelo preço unitário. Além disso, a tabela precisa conter um cabeçalho indicando as colunas: 'id', 'quantidade', 
 # 'preco' e 'total'.
 #Crie uma lista de tuplas em que cada tupla tenha id, quantidade, preço e valor total, na qual a primeira tupla é o cabeçalho da tabela.
-
+"""
 id = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 quantidade = [15, 12, 1, 15, 2, 11, 2, 12, 2, 4]
 preco = [93.0, 102.0, 18.0, 41.0, 122.0, 14.0, 71.0, 48.0, 14.0, 144.0]
+
+lista_de_tuplas = [('ID', 'QUANTIDADE','PREÇO', 'TOTAL')]
+lista_de_tuplas += [(id[i], quantidade[i], preco[i], quantidade[i] * preco[i]) for i in range(len(id))]
+
+for i in lista_de_tuplas:
+    print(i)
+"""
+
+# 9.Uma empresa possui filiais espalhadas nos Estados da região Sudeste do Brasil. Em uma das tabelas de cadastro das filiais 
+# há uma coluna contendo a informação de qual é o Estado a que pertence:
+# A empresa sempre está abrindo novas filiais, de modo que a tabela está constantemente recebendo novos registros e o gestor 
+# gostaria de possuir a informação atualizada da quantidade de filiais em cada Estado.
+# A partir da coluna com a informação dos Estados, crie um dicionário usando dict comprehension com a chave sendo o nome 
+# de um Estado e o valor sendo a contagem de vezes em que o Estado aparece na lista.
+
+estados = ['SP', 'ES', 'MG', 'MG', 'SP', 'MG', 'ES', 'ES', 'ES', 'SP', 'SP', 'MG', 'ES', 'SP', 'RJ', 'MG', 
+           'RJ', 'SP', 'MG', 'SP', 'ES', 'SP', 'MG']
+
 
--- Lidando com exceções.py
+++ Lidando com exceções.py
@@ -13,7 +13,88 @@ TypeError: aparece audno tentamos por exemplos somar uma string com inteiro
 
 KeyError: aparece quando tentamos acessa ruma chave que não existe no dicionario 
 
+
+Try:
+    # codigo a ser executado. Caso uma exeçção seja lançada, pare imediatamente
+exept <nome_da_exeção as e>:
+    # Se uma exeção for lançada no try, rode esse codigo, senão pule essa etapa
 """
+
+#Tratando exceções
+
+# Situação
+# Você criou um código que lê um dicionário com as notas dos estudantes e quis retornar a lista de notas de determinado(a) estudante. 
+# Caso o(a) estudante não esteja matriculado(a) na classe devemos tratar a exceção para aparecer a mensagem "Estudante não matriculado(a) 
+# na turma", e se a exceção não for lançada devemos exibir a lista com as notas do(a) estudante. Vamos trabalhar nesse exemplo com a exceção 
+# Key Error que interromperá o processo desse pedaço do código. Vamos testar esse tratamento?
+"""
+notas = {'João': [8.0, 9.0, 10.0], 'Maria': [9.0, 7.0, 6.0], 'José': [3.4, 7.0, 8.0],'Cláudia': [5.5, 6.6, 8.0], 'Ana': [6.0, 10.0, 9.5], 
+        'Joaquim': [5.5, 7.5, 9.0], 'Júlia': [6.0, 8.0, 7.0], 'Pedro': [3.0, 4.0, 6.0]}
+
+try: # oque voce vai fazer
+    nome = input("Digite o nome do(a) estudante: ")
+    resultado = notas[nome]
+except KeyError: # se oque fez deu o erro KeyError exibe ou faça isso
+    print("Estudante não matriculado(a) na turma")
+else: # se não deu erro faça isso
+    print(resultado)
+finally: # vai fazer sempre, se tiver erro ou não
+    print("A consulta foi encerrada!")
+"""
+
+
+# Raise
+
+# voce cria uma exeção para algum erro que não queira que aconteça
+# raise ValueError("A lista de notas deve possuir dez elementos")
+
+# Situação
+# Você criou uma função para calcular a média de um estudante em uma dada matéria passando em uma lista as notas deste estudante. 
+# Você pretende tratar 2 situações:
+# Se a lista possuir um valor não numérico o cálculo de média não serà executado e uma mensagem de "Não foi possível calcular a média 
+# do(a) estudante. Só são aceitos valores numéricos!" será exibida.
+# Caso a lista tenha mais de 4 notas, será lançada uma exceção do tipo ValueError informando que "A lista não pode possuir mais de 4 notas."
+# Um texto avisando que "A consulta foi encerrada!" deve ser exibido com ou sem a exceção ser lançada. 
+"""
+def media(lista: list=[0]) -> float:
+    calculo = sum(lista) / len(lista)
+
+    if len(lista) > 4:
+        raise ValueError("A lista não pode possuir mais de 4 notas.")
+    
+    return calculo
+
+try:
+    #notas = [6, 7, 8, 9, 10]  # da o erro criado a lista tem mais de 4 valores 
+    notas = [6, 7, 8, "9"]
+    resultado = media(notas)
+except TypeError:
+    print("Não foi possível calcular a média do(a) estudante. Só são aceitos valores numéricos!")
+except ValueError as e:
+    print(e)
+else:
+    print(resultado)
+finally:
+    print("A consulta foi encerrada!")
+"""
+
+# Exercicios
+
+# 1.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
